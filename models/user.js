c4de0b1a578d356b16aa61339d9bf53f26cc12f1
japc <japc.grafico@gmail.com>
2021-08-13 21:03:13 +0200

    Filter object output

--- models/user.js
+++ models/user.js
@@ -30,4 +30,11 @@ const UserSchema = new Schema({
   }
 })
 
+// Se filtra la salida del Objeto (se ignora password, __v )
+UserSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...user } = this.toObject();
+  user.uid = _id;
+  return user;
+}
+
 module.exports = model('User', UserSchema);
