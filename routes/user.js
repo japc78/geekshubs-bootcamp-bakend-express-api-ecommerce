fda303593f679c86211fd47ecaf27061050c1e75
japc <japc.grafico@gmail.com>
2021-08-16 11:58:03 +0200

    Add middleware JWT to user endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,25 @@
 const { Router } = require('express');
 const { getUserController, addUserController, updateUserController, deleteUserController} = require('../controllers/user');
 
+const { validJWT } = require('../middlewares/validJWT');
+
 const router = Router()
 
-router.get('/', getUserController);
+router.get('/',[
+  validJWT,
+], getUserController);
+
+
 router.post('/', addUserController);
-router.put('/:id', updateUserController);
-router.delete('/:id', deleteUserController);
+
+
+router.put('/:id',[
+  validJWT,
+], updateUserController);
+
+
+router.delete('/:id',[
+  validJWT,
+], deleteUserController);
 
 module.exports = router;
